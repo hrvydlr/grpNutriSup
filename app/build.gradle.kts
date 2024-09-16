87c849ed5718705a4fe7f0f810cd574be0535b4d
Aiseee <akihiro2903@yahoo.com>
2024-09-17 01:27:46 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-firestore:24.7.1")
-    implementation (libs.imageslideshow)
 
 }
